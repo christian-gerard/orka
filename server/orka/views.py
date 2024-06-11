1e6cd062e62fb7c4681155c6f8128c7c8f32b14b
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-06-11 17:38:33 -0600

    Added V1 Models

--- server/orka/views.py
+++ server/orka/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
+
+def user(request,user_id):
+    user_name = None
+    try:
+        if user_id == 1:
+            user_name = 'Bryce Defoe'
+        elif user_id == 2:
+            user_name = 'Jack Gerard'
+        return HttpResponseRedirect(user_name)
+    except:
+        return HttpResponseNotFound("User Not Found")
