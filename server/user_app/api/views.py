9e8a4912b9b0eab6bf4372b6c81ccc674d705f7e
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-06-20 18:09:11 -0600

    Added SignUp and Login

--- server/user_app/api/views.py
+++ server/user_app/api/views.py
@@ -5,14 +5,27 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from django.http import Http404
+from django.shortcuts import get_object_or_404
+from orka.models.Account import Account
+from orka.models.User import User
 from orka.api.serializers.UserSerializer import UserSerializer
+from orka.api.serializers.AccountSerializer import AccountSerializer
 from pdb import set_trace
 
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([AllowAny])
 def login(request):
-    return Response({"MESSAGRE": "MESSAGE"})
+    user = get_object_or_404(User, username=request.data['username'])
+
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+
+    return Response({'token': token.key, 'user': serializer.data})
+
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -21,11 +34,16 @@ def signup(request):
     serializer = UserSerializer(data=request.data)
 
     if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token,created = Token.objects.get_or_create(user=user)
 
-        return Response("AVALID")
+        return Response({"token": token.key, "user": serializer.data})
     
     
-    return Response(serializer.errors, 401)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def signout(request):
