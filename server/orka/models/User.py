b9e4f89cb51fa4762ba0852bbea00a1ad6da39fc
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-08-20 14:52:16 -0600

    Cleaning Up User Field'

--- server/orka/models/User.py
+++ server/orka/models/User.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
 
     password = models.CharField(
-        max_length=20
+        max_length=200
     )
 
     admin = models.BooleanField(default=False)
